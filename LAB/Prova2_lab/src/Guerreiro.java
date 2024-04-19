ff841c8747a1fe7cc21f4a0edbf02bc51aea7a94
FelipeZeferino <fzeferino16@gmail.com>
2024-04-19 13:34:27 -0300

    Added aulas

--- LAB/Prova2_lab/src/Guerreiro.java
+++ LAB/Prova2_lab/src/Guerreiro.java
@@ -1,17 +1,13 @@
 public class Guerreiro extends Personagem implements LutarComArmas{
 
      private Arma arma = new Arma("Espada longa", 50);
-
-
-
+     
     public Guerreiro(String nome, int vida, int energia, int poder) {
         super(nome, vida, energia, poder);
     }
     public void setArma(Arma arma) {
         this.arma = arma;
     }
-
-
 
     public void atacar(Personagem personagem) {
         System.out.println("Atacando..");
